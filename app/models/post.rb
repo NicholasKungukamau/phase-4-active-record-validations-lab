2860143ca95a9e9efcfaec5a00031506fdd412cc
NicholasKunguKamau <nicholaskungukamau@gmail.com>
2022-10-02 12:50:50 +0300

    commit tree 7453b4c9358b9459fff3aca57aa1a9c4fc56cd4b

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validates :title, exclusion: { in: ["True Facts"] }
+
+    validate :click_bate
+
+    def click_bate
+        unless title.match?(/Won't Believe|Secret|Top [0-9]|Guess/)
+            errors.add(:title, "Must contain clickbatey element")
+        end if title
+    end
 end
